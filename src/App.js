6664b1ed93d08462eabc73eb0e901d67c478e7d6
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2023-04-09 01:11:35 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/login/LoginPage";
+import { RegisterPage } from "./pages/login/RegisterPage";
+import { LeaderboardPage } from "./pages/leaderboard/LeaderboardPage";
+import { ProfilePage } from "./pages/profile/ProfilePage";
+import { ShowcasePage } from "./pages/showcase/ShowcasePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/leaderboard" element={<LeaderboardPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
+        <Route path="/showcase" element={<ShowcasePage />}></Route>
+      </Routes>
     </div>
   );
 }
