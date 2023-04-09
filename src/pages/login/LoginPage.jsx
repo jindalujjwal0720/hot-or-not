cdefe2cffa37d500920dbfc00f52e367c7e12d6d
Chandra\nPrakash <chandraprakash47637@gmail.com>
2023-04-09 11:46:50 +0530

    master

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+// import "./login.css"
+import "./log.css"
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="loginContainer">
+      <div className="new-account top">
+        <img src="/z.png" alt="Love You" id="love"/>
+      Flirt and meet new people
+      </div>
+      <div className="form-data">
+        <input type="text" className="form-input" placeholder="Username"/>
+        <input type="password" className="form-input" placeholder="Password"/>
+      <button className="form-submit">Login</button>
+      </div>
+      <div className="new-account">
+        Don't have an account?
+        <div className="create">Create</div>
+      </div>
+    </div>
+  );
+};
--- src/pages/login/RegisterPage.jsx
+++ src/pages/login/RegisterPage.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+// import "./login.css"
+import "./log.css"
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <div className="loginContainer">
+      <div className="new-account top">
+        <img src="/z.png" alt="Love You" id="love"/>
+      Flirt and meet new people
+      </div>
+      <div className="form-data">
+        <input type="text" className="form-input" placeholder="Username"/>
+        <input type="email" className="form-input" placeholder="Email"/>
+        <input type="password" className="form-input" placeholder="Password"/>
+      <button className="form-submit">Login</button>
+      </div>
+      <div className="new-account">
+        Already Signin?
+        <div className="create">Login</div>
+      </div>
+    </div>
+  );
+};
